1519d39750996cbcb66f5052501ce1a509400d17
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-03-21 21:12:34 +0530

    initialize project structure with Next.js, Tailwind CSS, and MongoDB setup

---
+++ my-app/lib/db.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+
+const MONGODB_URL = process.env.MONGODB_URL!;
+
+if (!MONGODB_URL) {
+    throw new Error("Please define mongodb url in env file")
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = {conn : null, promise : null};
+}
+
+export async function connectToDatabase() {
+    if (cached.conn) {
+        return cached.conn
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            buffeCommands : true,
+            maxPoolSize : 10
+        };
+
+    cached.promise = mongoose
+    .connect(MONGODB_URL, opts)
+    .then(() => mongoose.connection);
+
+    }
+
+    try {
+        cached.conn = await cached.promise
+    } catch (error) {
+        cached.promise = null
+        throw error
+    }
+
+    return cached.conn;
+}
